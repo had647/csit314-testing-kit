b0003cb3269698fea90fbfcbde87086600c43812
Huy Anh Dinh <had647@uowmail.edu.au>
2020-06-10 22:52:44 +1000

    Reimport the new data retrieval method for several test modules

--- Master/Test_Job_Salary_Range_Oracle.py
+++ Master/Test_Job_Salary_Range_Oracle.py
@@ -24,27 +24,37 @@ def run_test_salary_range():
     urlList = file.read()
     urlListObject = json.loads(urlList)
 
-    print("Test for all range is running. . .")
+    print("Test for all salary range is running. . .")
     testStillTrue = True
     for urlObject in urlListObject['testdata']:
         salaryType = getTotalResultsFound(urlObject['url'])
         salaryTypeJobsCount = extractTotalIntValue(salaryType)
         if(urlObject['expectedMinVal'] == 1):
             if(salaryTypeJobsCount >= urlObject['expectedMinVal']):
+                print("Finished testing salary type '" + urlObject['type'] + "' with range from " + str(urlObject['min']) + " to " + str(urlObject['max']) + "!")
                 outputCSV("Results retrieving jobs from salary type '" + urlObject['type'] + "' with range from " + str(urlObject['min']) + " to " + str(urlObject['max']), True, urlObject['expectedMinVal'], salaryTypeJobsCount)
             else:
-                testStillTrue = False
+                if(testStillTrue):
+                    testStillTrue = False
+                print("Finished testing salary type '" + urlObject['type'] + "' with range from " + str(urlObject['min']) + " to " + str(urlObject['max']) + "!")
                 outputCSV("Results retrieving jobs from salary type '" + urlObject['type'] + "' with range from " + str(urlObject['min']) + " to " + str(urlObject['max']), False, urlObject['expectedMinVal'],
                           salaryTypeJobsCount)
         elif(urlObject['expectedMinVal'] == 0):
             if (salaryTypeJobsCount == urlObject['expectedMinVal']):
+                print("Finished testing salary type '" + urlObject['type'] + "' with range from " + str(
+                    urlObject['min']) + " to " + str(urlObject['max']) + "!")
                 outputCSV("Results retrieving jobs from salary type '" + urlObject['type'] + "' with range from " + str(
                     urlObject['min']) + " to " + str(urlObject['max']), True, urlObject['expectedMinVal'],
                           salaryTypeJobsCount)
             else:
-                testStillTrue = False
+                if (testStillTrue):
+                    testStillTrue = False
+                print("Finished testing salary type '" + urlObject['type'] + "' with range from " + str(
+                    urlObject['min']) + " to " + str(urlObject['max']) + "!")
                 outputCSV("Results retrieving jobs from salary type '" + urlObject['type'] + "' with range from " + str(
                     urlObject['min']) + " to " + str(urlObject['max']), False, urlObject['expectedMinVal'],
                           salaryTypeJobsCount)
+    print("Test for all salary range is finished !")
     # 1 false -> the test is not success
     outputCSV("Results testing all salary ranges for Hourly Rate, Annual Salary (Package and Commission) in Gumtree", testStillTrue, 0, 1)
+    print("Finished Testing To Check Salary Range Oracle !")
