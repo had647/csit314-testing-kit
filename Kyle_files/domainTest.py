a5cdd699aaa4a3558c9cae94cff0f4c984a8949e
kylelw23 <dkb139@uowmail.edu.au>
2020-06-10 21:54:58 +1000

    Add Clothing and Jewellery Test

--- Kyle_files/domainTest.py
+++ Kyle_files/domainTest.py
@@ -1,5 +1,6 @@
 from Kyle_files.Test_Oracle_MISC import *
 from Kyle_files.Test_Oracle_RealEstate import *
+from Kyle_files.Test_Oracle_ClothingJewellery import *
 from Kyle_files.outputCSV import *
 
 def runAutomatedTest():
@@ -7,6 +8,7 @@ def runAutomatedTest():
     # All test oracles go here
     real_estate_search_test()
     misc_search_test()
+    clothing_jewellery_search_test()
 
 
 if __name__ == "__main__":
