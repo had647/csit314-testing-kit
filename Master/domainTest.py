7d305e991e66dcd91c970ccffa783412e8046096
Devoun <66659970+Devoun@users.noreply.github.com>
2020-06-10 23:21:18 +1000

    Update domainTest.py

--- Master/domainTest.py
+++ Master/domainTest.py
@@ -5,6 +5,7 @@ from Master.test_Oracle_AppropriateCategory import *
 from Master.test_oracle_categoryNumbers import *
 from Master.test_oracle_priceFilter import *
 from Master.test_oracle_freeListings import *
+from Master.test_oracle_priceType import * #Denver untested
 from Master.outputCSV import *
 
 # Here we can setup the headers once, then call all of our tests. Each test should handle the CSV ouput in its function.
@@ -18,6 +19,7 @@ def runAutomatedTest():
     run_test_CategoryNumber()
     run_test_PriceFilters()
     run_test_freeListing()
+    run_test_priceType()
 
 
 if __name__ == "__main__":
