498c9229c0b052bd637e32cb9d999a6f88faeaf8
Huy Anh Dinh <had647@uowmail.edu.au>
2020-06-11 01:41:17 +1000

    Modified returned value if response is detected

--- had647_files/Generic_Test_Lib.py
+++ had647_files/Generic_Test_Lib.py
@@ -18,9 +18,13 @@ def getTotalResultsFound(url):
         print("Encountered an error while trying to retrieve webpage from the following URL: \n" + url)
         print("\n\nERROR: Unknown BaseException")
     if response:
-        print("Response OK")
+        print("==================\n*Response Success*\n==================\n")
+        print("URL: " + url + "\n")
+        print("Response Code: " + str(response.status_code) + "\n")
     else:
-        print("Response Failed")
+        print("=================\n*Response Failed*\n=================\n")
+        print("URL: " + url + "\n")
+        print("Response Code: " + response.status_code + "\n")
     soup = BeautifulSoup(response.text, 'html.parser')
     results_list = soup.find(class_='breadcrumbs__summary--enhanced')
     result = results_list.get_text()
